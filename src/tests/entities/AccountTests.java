f5fac04335a95229292fd82effac665851e8781d
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-09-19 00:36:40 -0300

    Teste sobre depósito com quantia negativa

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -21,4 +21,16 @@ public class AccountTests {
 		// Assert: declare o que deveria acontecer (resultado esperado)
 		Assertions.assertEquals(expectedValue, acc.getBalance()); // É uma lib de declarações e possui vários métodos estáticos para fazermos as declarações
 	}
+	
+	@Test		// Quando a quantia informada for negativa o depósito não fará nada
+	public void depositShouldDoNothingWhenNegativeAmount() {
+		
+		double expectedValue = 100.0;
+		Account acc = new Account(1L, expectedValue);
+		double amount = -200.0;
+		
+		acc.deposit(amount);
+		
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
 }
